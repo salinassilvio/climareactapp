41061051b6a3d19b9dbc452bc9b811427c8b4554
Silvio Salinas Antón <ssalinas.silvio@gmail.com>
2020-07-21 00:36:41 +0200

    Se crean nuevo componente de formulario js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, {Fragment} from 'react';
+import Header from './components/Header';
+import Formulario from './components/Formulario';
 
 function App() {
   return (
-    <h1>Clima React</h1>
+    <Fragment>
+      <Header
+        titulo = 'Clima React App'
+      ></Header>
+      <div className="contenedor-form">
+        <div className="container">
+          <div className="row">
+              <div className=" col m6 s12">
+                  <Formulario></Formulario>
+              </div>
+              <div className=" col m6 s12">
+
+</div>
+          </div>
+        </div>
+      </div>
+    </Fragment>
+
   );
 }
 
